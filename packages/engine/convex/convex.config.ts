08815fdd11c8dc2a89f96fbe5cea4a6a8c1f79d9
jesse <the.jesse.dev@gmail.com>
2026-03-15 13:52:12 -0700

    removed workflow component

--- packages/engine/convex/convex.config.ts
+++ packages/engine/convex/convex.config.ts
@@ -1,9 +1,7 @@
 import { defineApp } from "convex/server";
 import rateLimiter from "@convex-dev/rate-limiter/convex.config";
-import workflows from "@convex-dev/workflow/convex.config";
 
 const app = defineApp();
 app.use(rateLimiter);
-app.use(workflows);
 
 export default app;
